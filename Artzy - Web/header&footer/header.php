bca93d7ac0fe3f3a191b925f1ef7e4ec943ff71c
Miles <vegasfh07@gmail.com>
2024-03-18 17:18:01 -0300

    wa

--- Artzy - Web/header&footer/header.php
+++ Artzy - Web/header&footer/header.php
@@ -1,11 +1,32 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="br">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>header</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    
+    <nav class="not-logged">
+        <div class="menu">
+            <ul> <!-- mobile --> 
+
+            </ul>
+            <ul class="menu_">
+                <li class="menu-logo">
+                    <a href="http://localhost/T14_PHP/Artzy%20-%20Web/header&footer/header.php">
+                        <img src="src/logo.png" class="logo" >
+                    </a>
+                </li>
+                <li class="menu-nav dropdown">
+                    <button class="menu-nav dropdown-toggle">
+                        <span class="Explorar">
+                            Explorar
+                        </span>
+                    </button>
+                </li>
+            </ul>
+        </div>
+    </nav>
 </body>
 </html>
