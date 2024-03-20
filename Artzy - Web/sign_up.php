eab72f641d047984aa6cc19472b6cb0ec58e92a9
Miles <128197330+ryujin-n@users.noreply.github.com>
2024-03-20 17:16:00 -0300

    up

--- Artzy - Web/sign_up.php
+++ Artzy - Web/sign_up.php
@@ -3,9 +3,23 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <link rel="stylesheet" href="css/style-cadastro.css">
     <title>Artzy - Cadastro</title>
 </head>
 <body>
-    
+<div class="fundo">
+        <div class="lado-esquerdo">
+            <div class="fundo2">
+                <!-- <img src="src/pijm.png" class="pin" alt="" id="p1">
+                <img src="src/pijm.png" class="pin" alt="" id="p2"> -->
+            </div>
+        </div>
+        <div class="lado-direito">
+            <div class="log-fund">
+              
+            </div>
+        </div>
+    </div>
 </body>
 </html>
