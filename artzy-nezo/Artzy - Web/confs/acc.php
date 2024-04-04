37c71dedcbea4c0d9402de1f0d567641d5d11ff2
NelsonSaless <138810377+NelsonSaless@users.noreply.github.com>
2024-04-04 17:19:05 -0300

    wawa

--- artzy-nezo/Artzy - Web/confs/acc.php
+++ artzy-nezo/Artzy - Web/confs/acc.php
@@ -1,52 +1,79 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+
+    
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link rel="stylesheet" href="acc.css">
 </head>
 <body>
-    <div class="cont-tit">
-        <span class="conts">
-            conta
-        </span>
-    </div>
 
-    <div class="conta">
-        <span class="Email">
-            Email
-        </span>
+<?php include_once('del_acc.php')?>
 
-        <input type="text" class="cont-inp">
+    <form action="" method="post" onsubmit="return:false;" id="acc" name="acc">
 
-        <button class="enviar3">
-            <i class="fi fi-br-file-upload e-i2"></i>
-            <span>
-                Salvar
+        <div class="cont-tit">
+            <span class="conts">
+                conta
             </span>
-        </button>
-
-        <div class="Line-29"></div>
-
-        <span class="rea-perigosa">
-            área perigosa
-        </span>
-
-        <span class="alerta">
-        Observe que, depois de remover sua conta, todas as obras de arte que você postou serão excluídas
-        </span>
-
-        <button class="Deletar-conta">
-            <i class="fi fi-br-file-upload d-i"></i>
-            <span>
-                Deletar conta
+        </div>
+    
+        <div class="conta">
+            <span class="Email">
+                Email
             </span>
-        </button>
-        
-    </div>
+    
+            <input type="text" class="cont-inp">
+    
+            <button class="enviar3">
+                <i class="fi fi-br-file-upload e-i2"></i>
+                <span>
+                    Salvar
+                </span>
+            </button>
+    
+            <div class="Line-29"></div>
+    
+            <span class="rea-perigosa">
+                área perigosa
+            </span>
+    
+            <span class="alerta">
+            Observe que, depois de remover sua conta, todas as obras de arte que você postou serão excluídas
+            </span>
+    
+            <button class="Deletar-conta" id="del" name="del" onclick="del()">
+                <i class="fi fi-br-file-upload d-i"></i>
+                <span>
+                    Deletar conta
+                </span>
+            </button>
+        </div>
 
-   
+        <input type="text" 
+        style="
+            position: absolute; 
+            top: 5.2rem;
+            left: 9rem;
+            /* display:none; */
+            
+        "
+        name="perfill"
+        id="perfill"
+        value="<?=$id?>">
+
+    </form>
+
+    <script>
+        function enviar(tipo) {
+            
+            const form =document.getElementById("acc")
+            form.submit()
+        }
+
+    </script>
 
 </body>
 </html>
--- artzy-nezo/Artzy - Web/header&footer/header_signedon.php
+++ artzy-nezo/Artzy - Web/header&footer/header_signedon.php
@@ -4,13 +4,11 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>header</title>
+    <title>wa</title>
     <link rel="stylesheet" href="css/bootstrap.css">
     <link rel="stylesheet" href="header&footer/style2.css">
-    <link rel='stylesheet'
-        href='https://cdn-uicons.flaticon.com/2.2.0/uicons-solid-rounded/css/uicons-solid-rounded.css'>
-    <link rel='stylesheet'
-        href='https://cdn-uicons.flaticon.com/2.2.0/uicons-regular-rounded/css/uicons-regular-rounded.css'>
+    <link rel='stylesheet' href='https://cdn-uicons.flaticon.com/2.2.0/uicons-solid-rounded/css/uicons-solid-rounded.css'>
+    <link rel='stylesheet' href='https://cdn-uicons.flaticon.com/2.2.0/uicons-regular-rounded/css/uicons-regular-rounded.css'>
     <link rel='stylesheet' href='https://cdn-uicons.flaticon.com/2.2.0/uicons-bold-rounded/css/uicons-bold-rounded.css'>
 
 </head>
